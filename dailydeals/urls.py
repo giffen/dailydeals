acbee356a616d6910cdb8eb3ff49b5a35457db5d
Andy Giffen <giffen@gmail.com>
2014-04-27 08:17:45 -0700

    Add home page and bootstrap

--- dailydeals/urls.py
+++ dailydeals/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+		url(r'^$', 'dailydeals.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
---
+++ dailydeals/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response, RequestContext
+
+def home(request):
+	
+	return render_to_response("home.html", locals(), context_instance=RequestContext(request))
