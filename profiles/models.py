7f8251327e20f15a55c96ec63ac01cb22bd53017
Andy Giffen <giffen@gmail.com>
2014-04-28 09:02:23 -0700

    Add Stripe to model

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in
+
+from dailydeals.stripe_info import secret_key
+
+#how to connect to the API
+import stripe
+stripe.api_key = secret_key
 
 class UserStripe(models.Model):
 	user = models.OneToOneField(User)
@@ -9,4 +16,14 @@ class UserStripe(models.Model):
 	def __unicode__(self):
 		return self.user.username
 
-
+def CreateStripeId(sender, user, request, **kwargs):
+	new_id, created = UserStripe.objects.get_or_create(user=user)
+	if created:
+		#add users email to stripe, then set stripe id to model
+		stripe_cust = stripe.Customer.create(email=user.email, description='Customer %s was created' %(user.username))
+		print stripe_cust.id
+		new_id.stripe_id = stripe_cust.id
+		new_id.save()
+	else:
+		print "Stripe has been created %s" %new_id	
+user_logged_in.connect(CreateStripeId)
