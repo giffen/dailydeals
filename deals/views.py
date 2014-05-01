010987e258c8ebb563add4711a116e06f14efee0
Andy Giffen <giffen@gmail.com>
2014-05-01 07:43:57 -0700

    Add pageination to archive

--- deals/views.py
+++ deals/views.py
@@ -1,9 +1,22 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, Http404
 
 from .models import Deal
 
 def all_deals(request):
-	posts = Deal.objects.filter(featured=True)
+	all_posts = Deal.objects.filter(featured=True)
+	paginator = Paginator(all_posts, 2) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		posts = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		posts = paginator.page(paginator.num_pages)
+
 	return render(request, 'deals/all.html', locals())
 
 
